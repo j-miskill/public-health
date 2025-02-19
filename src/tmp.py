c0fa91529da83f2c114066ead8270e3f4e988944
j-miskill <jcmiskill@gmail.com>
2025-02-19 12:31:59 -0500

    youtube data is huge

--- src/tmp.py
+++ src/tmp.py
@@ -5,9 +5,10 @@ from pprint import pprint
 if __name__ == "__main__":
     directory = "../takeout-files"
     tp = TakeoutParser(directory)
-    file_path = "../takeout-files/Ads.html"
+    file_path = "../takeout-files/YouTube.html"
 
-    df= tp.parse_html(file_path)
+    df = tp.parse_html(file_path)
+
     pprint(df)
 
 
