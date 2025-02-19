bd64164cd09b8a50fd8db194005238774ee20dfb
j-miskill <jcmiskill@gmail.com>
2025-02-19 17:32:52 -0500

    started coding the analysis

--- src/run_job.py
+++
@@ -1,8 +0,0 @@
-from scrape_timeline_data import TakeoutParser
-
-if __name__ == "__main__":
-    print("Please provide the directory")
-    directory = "../takeout-files"
-    tp = TakeoutParser(directory=directory)
-    tp.scrape_all_takeout_files()
-
