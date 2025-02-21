6cb2e46832ac1e876ff7f5a01126279b53e5c4bc
j-miskill <jcmiskill@gmail.com>
2025-02-20 22:24:09 -0500

    changes for the evening. Having thoughts about automating summary statistics for this work

--- src/Analyzer.py
+++ src/Analyzer.py
@@ -1,62 +1,9 @@
-import pandas as pd
-import numpy as np
-from pprint import pprint
-import matplotlib.pyplot as plt
-import seaborn as sns
+"""
+    I will come back to this, but the purpose would be to automate summary statistics, given a dataset.
 
-if __name__ == "__main__":
-    takeout_data = pd.read_csv("output-data/takeout.csv")
-    st_data = pd.read_csv("output-data/app-usage-data.csv")
-
-    takeout_data['date'] = pd.to_datetime(takeout_data['date'])
-    takeout_data['time'] = pd.to_datetime(takeout_data['time'])
-    # st_data['datetime'] = pd.to_datetime(st_data['date'] + ' ' + st_data['time'])
-
-
-
-    # starting with the takeout data
-    # I could start with a histogram, though
-    # maybe for the earliest times I should remove all values after noon, because that would be an outlier
-    # maybe for the latest times, I should remove all values before 5pm, because that would also be an outlier
-
-    earliest_times = takeout_data.groupby("date")['time'].min()
-    latest_times = takeout_data.groupby("date")['time'].max()
-
-    pprint(earliest_times)
-
-    earliest_seconds = earliest_times.dt.hour * 3600 + earliest_times.dt.minute * 60 + earliest_times.dt.second
-    latest_seconds = latest_times.dt.hour * 3600 + latest_times.dt.minute * 60 + latest_times.dt.second
-
-    plt.figure(figsize=(10, 6))
-    sns.histplot(earliest_seconds, kde=True, color="blue", label="Earliest Times", stat="density", bins=10, linewidth=2)
-    sns.histplot(latest_seconds, kde=True, color="red", label="Latest Times", stat="density", bins=10, linewidth=2)
-
-    # Add labels and title
-    plt.xlabel('Time of Day (seconds since midnight)')
-    plt.ylabel('Density')
-    plt.title('Histogram of Earliest and Latest Times of Day')
-    plt.legend()
-
-    # Show the plot
-    plt.show()
-
-    avg_earliest_seconds = earliest_seconds.mean()
-    avg_latest_seconds = latest_seconds.mean()
-
-    # Step 4: Convert back to hours, minutes, and seconds
-    avg_earliest_time = pd.to_datetime(avg_earliest_seconds, unit='s').time()
-    avg_latest_time = pd.to_datetime(avg_latest_seconds, unit='s').time()
-
-    # print("Earliest time", avg_earliest_time)
-    # print("Latest time", avg_latest_time)
-
-
-
-
-
-
-
-
-
-
-
+    Summary statistics:
+        1. Number of records
+        2. length between first date and last date
+        3. distinct days I have knowledge on
+        4.
+"""
---
+++ src/analysis.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import numpy as np
+from pprint import pprint
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+if __name__ == "__main__":
+    takeout_data = pd.read_csv("output-data/takeout.csv")
+    st_data = pd.read_csv("output-data/app-usage-data.csv")
+
+    takeout_data['date'] = pd.to_datetime(takeout_data['date'])
+    takeout_data['time'] = pd.to_datetime(takeout_data['time'])
+    # st_data['datetime'] = pd.to_datetime(st_data['date'] + ' ' + st_data['time'])
+
+    min_date = takeout_data['date'].min()
+    max_date = takeout_data['date'].max()
+    diff = max_date-min_date
+
+    print(f"First date: {min_date}")
+    print(f"Last date: {max_date}")
+
+    print(f"Size: {takeout_data.shape}")
+
+    print(f"Number of days in between first and last dates {diff.days}")
+
+    num_distinct_days = takeout_data['date'].unique()
+    print(f"Distinct days: {num_distinct_days.shape[0]}")
+
+    day_with_most_information = takeout_data.groupby('date').count()
+
+    print(takeout_data.loc(takeout_data['date'] == "2018-01-18"))
+
+    print('most information:', day_with_most_information)
+
+
+
+    # starting with the takeout data
+    # I could start with a histogram, though
+    # maybe for the earliest times I should remove all values after noon, because that would be an outlier
+    # maybe for the latest times, I should remove all values before 5pm, because that would also be an outlier
+
+    # earliest_times = takeout_data.groupby("date")['time'].min()
+    # latest_times = takeout_data.groupby("date")['time'].max()
+    #
+    # pprint(earliest_times)
+    #
+    # earliest_seconds = earliest_times.dt.hour * 3600 + earliest_times.dt.minute * 60 + earliest_times.dt.second
+    # latest_seconds = latest_times.dt.hour * 3600 + latest_times.dt.minute * 60 + latest_times.dt.second
+    #
+    # plt.figure(figsize=(10, 6))
+    # sns.histplot(earliest_seconds, kde=True, color="blue", label="Earliest Times", stat="density", bins=10, linewidth=2)
+    # sns.histplot(latest_seconds, kde=True, color="red", label="Latest Times", stat="density", bins=10, linewidth=2)
+    #
+    # # Add labels and title
+    # plt.xlabel('Time of Day (seconds since midnight)')
+    # plt.ylabel('Density')
+    # plt.title('Histogram of Earliest and Latest Times of Day')
+    # plt.legend()
+
+    # Show the plot
+    # plt.show()
+    #
+    # avg_earliest_seconds = earliest_seconds.mean()
+    # avg_latest_seconds = latest_seconds.mean()
+    #
+    # # Step 4: Convert back to hours, minutes, and seconds
+    # avg_earliest_time = pd.to_datetime(avg_earliest_seconds, unit='s').time()
+    # avg_latest_time = pd.to_datetime(avg_latest_seconds, unit='s').time()
+
+    # print("Earliest time", avg_earliest_time)
+    # print("Latest time", avg_latest_time)
+
+
+
+
+
+
+
+
+
+
+
